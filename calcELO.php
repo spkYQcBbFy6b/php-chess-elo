55e28536c5b96f9243b7d259447df8248c8cbc6a
spkYQcBbFy6b <46068118+spkYQcBbFy6b@users.noreply.github.com>
2019-05-17 02:58:00 +0200

    Add files via upload

---
+++ calcELO.php
@@ -0,0 +1,14 @@
+<?php
+
+function FN_calcELO($elo1, $elo2, $result, $k=20)
+{
+ $newELO1 = round($elo1 + $k * ( $result - (1 / ( 1 + pow(10, ($elo22 - $elo1)/400) )) ));
+ $newELO2 = $elo2 + ($elo1 - $newELO1);
+ return(array($newELO1, $newELO2);
+}
+
+// $elo1: should be white
+// $elo2: should be black
+// $result: seen from $elo1: 1=won, 0=lost, remis=0.5
+// $k: normally 20, for some exceptions there are informations to find on the net.
+?>
