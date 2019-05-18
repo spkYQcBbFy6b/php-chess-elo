4b3f043e473d14460dad428e43356941dd10065e
spkYQcBbFy6b <46068118+spkYQcBbFy6b@users.noreply.github.com>
2019-05-18 06:46:19 +0200

    Update calcELO.php

--- calcELO.php
+++ calcELO.php
@@ -4,7 +4,7 @@ function FN_calcELO($elo1, $elo2, $result, $k=20)
 {
  $newELO1 = round($elo1 + $k * ( $result - (1 / ( 1 + pow(10, ($elo2 - $elo1)/400) )) ));
  $newELO2 = $elo2 + ($elo1 - $newELO1);
- return(array($newELO1, $newELO2);
+ return(array($newELO1, $newELO2));
 }
 
 // $elo1: should be white
